ee3edfca336122f33142d2be2c1a43d7717d5bdb
Daniel Aguilar <dacmail@gmail.com>
2015-06-16 15:46:50 +0200

    Destacado secundario y progress bar

--- js/default.js
+++ js/default.js
@@ -6,13 +6,19 @@
 		//Slideshow
 		var slideLoaded = imagesLoaded($('.slideshow'));
 		slideLoaded.on('done', configureSlide);
-		$('.slideshow').owlCarousel({
+		
+ 
+		
+
+		$elem = $('.slideshow').owlCarousel({
 			items:1,
-			loop:false,
+			loop:true,
 			center:true,
 			margin:10,
 			URLhashListener:true,
-			onInitialized: configureSlide
+			onInitialized: progressBar,
+			onTranslated : moved,
+			onDragged : pauseOnDragging
 		});
 	});
 	$(window).load(function() {
@@ -21,4 +27,52 @@
 	function configureSlide() {
 		$('.slideshow-wrap .col-sm-3').height($('.slideshow-wrap .col-sm-9').height());
 	}
+
+	var time = 15; // time in seconds
+	var $progressBar,
+		$bar, 
+		$elem, 
+		isPause, 
+		tick,
+		percentTime;
+
+	function progressBar(event) {
+		$progressBar = $("<div>",{id:"progressBar"});
+		$bar = $("<div>",{id:"bar"});
+		$progressBar.append($bar).appendTo($('.slideshow-wrap'));
+		start();
+	}
+    function start() {
+      //reset timer
+      percentTime = 0;
+      isPause = false;
+      //run interval every 0.01 second
+      tick = setInterval(interval, 20);
+    };
+ 
+    function interval() {
+      if(isPause === false){
+        percentTime += 1 / time;
+        $bar.css({
+           width: percentTime+"%"
+         });
+        if(percentTime >= 100) {
+        	moved();
+          	$elem.trigger('next.owl.carousel')
+        }
+      }
+    }
+ 
+    //pause while dragging 
+    function pauseOnDragging(){
+      isPause = true;
+    }
+ 
+    //moved callback
+    function moved(){
+      //clear interval
+      clearTimeout(tick);
+      //start again
+      start();
+    }
 })(jQuery);
--- templates/cat-secondary-featured.php
+++ templates/cat-secondary-featured.php
@@ -1,42 +1,30 @@
-<div class="row">
-	<?php $featured = new WP_Query(array('tax_query' => array(
-										array(
-											'taxonomy' => $term->taxonomy,
-											'field'    => 'term_id',
-											'terms'    => array($term->term_id),
-										)
-									),
-									'meta_key' => '_ungrynerd_cat_featured', 
-									'meta_value' => 1, 
-									'posts_per_page' => 3)); ?>
-	<div class="col-sm-9">
-		<div class="slideshow">
-		<?php while ($featured->have_posts()) : $featured->the_post(); ?>
-			<?php $posts_excluded[] = get_the_ID();  ?>
-			<article <?php post_class('slide-item'); ?> data-hash="<?php the_slug(); ?>">
-				<div class="image"><?php the_post_thumbnail('slideshow', array('class' => 'img-responsive')); ?></div>
-				<div class="title-wrap">
-					<h2 class="post-title">
-						<a href="<?php the_permalink() ?>" title="Enlace a <?php the_title_attribute(); ?>">
-							<?php the_title(); ?>
-						</a>
-					</h2>
-				</div>
-			</article>
-		<?php endwhile; ?>
+<?php $featured = new WP_Query(array('tax_query' => array(
+											array(
+												'taxonomy' => $term->taxonomy,
+												'field'    => 'term_id',
+												'terms'    => array($term->term_id),
+											)
+										),
+										'meta_key' => '_ungrynerd_cat_featured2', 
+										'meta_value' => 1, 
+										'posts_per_page' => 1)); ?>
+<?php if ($featured->have_posts()) : ?>
+	<div class="secondary-cat-featured">
+		<div class="row">
+			<?php while ($featured->have_posts()) : $featured->the_post(); ?>
+				<?php $posts_excluded[] = get_the_ID();  ?>
+				<article <?php post_class(); ?>>
+					<div class="image"><?php the_post_thumbnail('cat-secondary', array('class' => 'img-responsive')); ?></div>
+					<div class="title-wrap">
+						<h2 class="post-title">
+							<a href="<?php the_permalink() ?>" title="Enlace a <?php the_title_attribute(); ?>">
+								<?php the_title(); ?>
+							</a>
+						</h2>
+					</div>
+				</article>
+			<?php endwhile; ?>
+			<?php wp_reset_query(); ?>
 		</div>
 	</div>
-	<div class="col-sm-3">
-		<ul class="slideshow-nav">
-		<?php while ($featured->have_posts()) : $featured->the_post(); ?>
-			<?php $posts_excluded[] = get_the_ID();  ?>
-			<li>
-				<a href="#<?php the_slug(); ?>">
-					<?php the_title(); ?>
-				</a>
-			</li>
-		<?php endwhile; ?>
-		</ul>
-	</div>
-	<?php wp_reset_query(); ?>
-</div>
+<?php endif; ?>
