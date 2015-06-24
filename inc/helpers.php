54e79b8c21802d0c4effbc83e548e8805c0bfae6
Daniel Aguilar <dacmail@gmail.com>
2015-06-24 13:26:40 +0200

    Listados

--- inc/helpers.php
+++ inc/helpers.php
@@ -115,4 +115,39 @@
 		do_action('after_slug', $slug);
 		return $slug;
 	}
+
+	//Títulos de páginas
+	function ungrynerd_title() {
+		if (is_home()) {
+			if (get_option('page_for_posts', true)) {
+				return get_the_title(get_option('page_for_posts', true));
+			} else {
+			return __('Últimos artículos', 'wsl');
+			}
+		} elseif (is_archive()) {
+			$term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
+			if ($term) {
+				return apply_filters('single_term_title', $term->name);
+			} elseif (is_post_type_archive()) {
+				return apply_filters('the_title', get_queried_object()->labels->name);
+			} elseif (is_day()) {
+				return sprintf(__('Archivo diario: %s', 'wsl'), get_the_date());
+			} elseif (is_month()) {
+				return sprintf(__('Archivo mensual: %s', 'wsl'), get_the_date('F Y'));
+			} elseif (is_year()) {
+				return sprintf(__('Archivo anual: %s', 'wsl'), get_the_date('Y'));
+			} elseif (is_author()) {
+				$author = get_queried_object();
+				return sprintf(__('Artículos del autor: %s', 'wsl'), $author->display_name);
+			} else {
+				return single_cat_title('', false);
+			}
+		} elseif (is_search()) {
+			return sprintf(__('Resultados de búsqueda para: %s', 'wsl'), get_search_query());
+		} elseif (is_404()) {
+			return __('No encontrado', 'wsl');
+		} else {
+			return get_the_title();
+		}
+	}
 ?>
