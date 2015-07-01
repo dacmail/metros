71441e25553c5fdc6d180f252adee188280c58dc
Daniel Aguilar <dacmail@gmail.com>
2015-07-01 12:38:14 +0200

    Error 404

--- 404.php
+++ 404.php
@@ -1,8 +1,16 @@
 <?php get_header() ?>
-<div id="container" <?php body_class('clearfix'); ?>>
-	<section id="content" class="clearfix">
-		<h1 class="post-title"><?php _e('La p&aacute;gina que buscas no existe', 'ungrynerd'); ?></h1>
-		<p class="post-content"><a href="<?php echo home_url(); ?>"><?php _e('Vuelve a la p&aacute;gina de inicio', 'ungrynerd'); ?></a></p>
-	</section>
+<div id="container" <?php body_class('container clearfix'); ?>>
+	<div class="row">
+		<section class="content-404 col-sm-10 col-sm-offset-1">
+			<h2 class="block-title"><?php _e('Lo sentimos, la página que intentas ver no existe.', 'ungrynerd') ?></h2>
+			<p><?php _e('Puede ser que se deba a uno de los siguientes motivos:', 'ungrynerd'); ?></p>
+			<ul>
+				<li><?php _e('Una dirección mal escrita', 'ungrynerd'); ?></li>
+				<li><?php _e('Un enlace caducado', 'ungrynerd'); ?></li>
+			</ul>
+			<p><?php _e('Si quieres puedes usar el buscador:', 'ungrynerd'); ?></p>
+			<?php get_search_form(); ?>
+		</section>
+	</div>
 </div>
 <?php get_footer() ?>
