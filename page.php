35cf6b4d6ac5b3ad90686ac1ccb56a51b49d33e3
Daniel Aguilar <dacmail@gmail.com>
2015-07-01 12:10:46 +0200

    Página estática

--- page.php
+++ page.php
@@ -1,8 +1,24 @@
 <?php get_header() ?>
 <div id="container" class="clearfix">
-	<section id="content" class="clearfix">
-		<?php get_template_part( 'loop', 'single' ); ?>
-	</section>
-	<?php get_sidebar() ?>
+	<div class="container">
+		<div class="row">
+			<?php while (have_posts()) : the_post(); ?>
+			<div class="col-sm-9 sidebar-separator">
+				<article <?php post_class(); ?>>
+					<h1 class="block-title"><?php the_title(); ?></h1>
+					<?php if (has_post_thumbnail()): ?>
+						<div class="featured-img">
+							<?php the_post_thumbnail('featured-big', array('class' => 'img-responsive')); ?>
+						</div>
+					<?php endif ?>
+					<div class="post-content">
+						<?php the_content(); ?>
+					</div>
+				</article>
+			</div>
+			<?php endwhile; ?>
+			<?php get_sidebar('home') ?>
+		</div>
+	</div>
 </div>
 <?php get_footer() ?>
