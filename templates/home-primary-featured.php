fef5c37bb6a13e9858ae4230abc91206f153e970
Daniel Aguilar <dacmail@gmail.com>
2015-09-16 10:01:35 +0200

    Destacado principal

--- templates/home-primary-featured.php
+++ templates/home-primary-featured.php
@@ -3,20 +3,22 @@
 	<?php $video = get_post_meta(get_the_ID(), '_ungrynerd_recurso_video', true); ?>
 	<div <?php post_class('image-wrap ' . (!empty($video) ? 'video' : 'chart')); ?>>
 		<div class="container">
-			<?php the_post_thumbnail('featured-big'); ?>
-			<?php if (!empty($video)): ?>
-				<a href="#" class="play"></a>
-				<div class="video-wrap">
-					<?php echo wp_oembed_get($video); ?>
+			<div class="col-sm-12 wrapper">
+				<?php the_post_thumbnail('featured-big'); ?>
+				<?php if (!empty($video)): ?>
+					<a href="#" class="play"></a>
+					<div class="video-wrap">
+						<?php echo wp_oembed_get($video); ?>
+					</div>
+				<?php endif ?>
+				<div class="title-wrap">
+					<h2 class="post-title">
+						<a href="<?php the_permalink() ?>" title="Enlace a <?php the_title_attribute(); ?>">
+							<?php the_title(); ?>
+						</a>
+					</h2>
+					<p class="author-meta"><?php the_author_posts_link(); ?></p>
 				</div>
-			<?php endif ?>
-			<div class="title-wrap">
-				<h2 class="post-title">
-					<a href="<?php the_permalink() ?>" title="Enlace a <?php the_title_attribute(); ?>">
-						<?php the_title(); ?>
-					</a>
-				</h2>
-				<p class="author-meta"><?php the_author_posts_link(); ?></p>
 			</div>
 		</div>
 	</div>
